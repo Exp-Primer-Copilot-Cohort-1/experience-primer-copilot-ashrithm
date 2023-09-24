74b370dd33ebecbe546f6bf1105256bce117af48
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2023-09-24 17:14:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,112 @@
+function skillsMember() {
+    var skills = [
+        {
+            "name": "HTML",
+            "value": 90
+        },
+        {
+            "name": "CSS",
+            "value": 90
+        },
+        {
+            "name": "Javascript",
+            "value": 80
+        },
+        {
+            "name": "PHP",
+            "value": 70
+        },
+        {
+            "name": "MySQL",
+            "value": 70
+        },
+        {
+            "name": "C",
+            "value": 60
+        },
+        {
+            "name": "C++",
+            "value": 60
+        },
+        {
+            "name": "Java",
+            "value": 50
+        },
+        {
+            "name": "Python",
+            "value": 50
+        },
+        {
+            "name": "Ruby",
+            "value": 50
+        },
+        {
+            "name": "C#",
+            "value": 50
+        },
+        {
+            "name": "Swift",
+            "value": 50
+        },
+        {
+            "name": "Objective-C",
+            "value": 50
+        },
+        {
+            "name": "Go",
+            "value": 50
+        },
+        {
+            "name": "Scala",
+            "value": 50
+        },
+        {
+            "name": "Perl",
+            "value": 50
+        },
+        {
+            "name": "R",
+            "value": 50
+        },
+        {
+            "name": "Haskell",
+            "value": 50
+        },
+        {
+            "name": "Kotlin",
+            "value": 50
+        },
+        {
+            "name": "Rust",
+            "value": 50
+        },
+        {
+            "name": "Lua",
+            "value": 50
+        },
+        {
+            "name": "TypeScript",
+            "value": 50
+        },
+        {
+            "name": "CoffeeScript",
+            "value": 50
+        },
+        {
+            "name": "Shell",
+            "value": 50
+        },
+        {
+            "name": "PowerShell",
+            "value": 50
+        },
+        {
+            "name": "Erlang",
+            "value": 50
+        },
+        {
+            "name": "Clojure",
+            "value":40
+        }
+    ]}
+    return skills;
